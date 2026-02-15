19292d609403bc218a657fb6b824f346fe324cdf
Niskalaa <ryyuma60@gmail.com>
2026-02-15 12:59:06 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import GeneratorPage from "./pages/GeneratorPage.jsx";
+
 import StudioPage from "./pages/Studio.jsx";
+import GeneratorPage from "./pages/GeneratorPage.jsx";
+
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        {/* default */}
         <Route path="/" element={<Navigate to="/studio" replace />} />
+
+        {/* main app */}
         <Route path="/studio" element={<StudioPage />} />
+
+        {/* optional legacy */}
         <Route path="/generator" element={<GeneratorPage />} />
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/studio" replace />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
